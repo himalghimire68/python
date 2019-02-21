f58260506eec4e838f1f5cc7b1f82e87117057ab
himalghimire68 <ghimirehimal68@gmail.com>
2019-02-21 19:10:17 +0545

    Api and flask

---
+++ gui/flask/dic.py
@@ -0,0 +1,28 @@
+dict_all = [    {
+        'name': 'himal',
+        'roll': 21,
+        'place':'nepal',
+},
+{
+    'name': 'Sakar',
+    'roll': 22,
+    'place':'nepal',
+    },
+{
+    'name': 'Asok',
+    'roll': 23,
+    'place':'nepal',
+},
+{
+    'name': 'sabin',
+    'roll': 24,
+    'place':'nepal',
+},
+{
+    'name': 'himal',
+    'roll': 25,
+    'place':'nepal'
+}
+]
+
+print(dict_all)
